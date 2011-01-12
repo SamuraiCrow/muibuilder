d00ecd6937d4eff4b053b8762ad56855fea1bec6
marust <marust@b7be4560-88af-43e6-8048-5e6776c84f9e>
2011-01-12 19:14:41 +0000

    Fix for a segfault which happened during adding notifications while menu building.
    git-svn-id: https://svn.code.sf.net/p/muibuilder/code@75 b7be4560-88af-43e6-8048-5e6776c84f9e

--- v2/trunk/src/notify.c
+++ v2/trunk/src/notify.c
@@ -146,7 +146,9 @@ void LinkNotify(int depart)
 
 window *ParentWindow(APTR obj)
 {
-    // FIXME: I get crashes because obj == NULL
+    if (obj == NULL) // FIX for a segfault
+        return NULL;
+
     object *obj_aux;
 
     obj_aux = obj;
